02b14b4783139d47ce1633fed7a5ea558b50e87f
Lucas GUILHEN <lucas.guilhen67@gmail.com>
2022-05-10 22:36:42 +0200

    V3 - ajout de la fonction de calcul de quantité de fromage, et de la vérification des types de fromages sélectionnés

--- projets/fromages/script.js
+++ projets/fromages/script.js
@@ -1,16 +1,38 @@
+import * as blueCheeses from './cheeses/blue-cheese.json';
 
 function selected_event_type(eventType){
     switch (eventType){
         case 'repas':
-            document.getElementById('fromages-proposes').innerText = "L'événement est un V&F";
+            document.getElementById('event-type-selected').innerText = "L'événement est un V&F";
             break;
         case 'apero':
-            document.getElementById('fromages-proposes').innerText = "L'événement est un apéro";
+            document.getElementById('event-type-selected').innerText = "L'événement est un apéro";
             break;
         case 'degustation':
-            document.getElementById('fromages-proposes').innerText = "L'événement est un plateau de dégustation";
+            document.getElementById('event-type-selected').innerText = "L'événement est un plateau de dégustation";
             break;
-        default:
-            document.getElementById('fromages-proposes').innerText = "Veuillez sélectionner un type d'évenement";
     }
 }
+
+function cheese_types_quantity(nbPersonnes){
+    if (nbPersonnes <4){
+        return 3;
+    } else 
+    if (nbPersonnes < 15){
+        return ~~(nbPersonnes/3)*3;
+    } else 
+        return 15;
+}
+
+function check_cheese_types_selection(){
+    var checkboxes = document.getElementsByName('cheese-type');
+    var cheeses = [];
+    for (var i = 0; i < checkboxes.length; i++){
+        if (checkboxes[i].checked == true){
+            cheeses.push(checkboxes[i].value);
+        }
+    }
+    console.log(cheeses);
+}
+
+console.log(blueCheeses);
