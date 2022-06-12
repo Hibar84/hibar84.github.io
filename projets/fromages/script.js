a1649d3d60128c203f89aa6d657a7eeeab6483db
Hibar84 <lucas.guilhen67@gmail.com>
2022-06-12 15:54:13 +0200

    stockage des fromages dans un fichier annexe, finalisation de la génération du plateau, début de stylisation CSS

--- projets/fromages/script.js
+++ projets/fromages/script.js
@@ -2,160 +2,20 @@ let eventSelected; // type d'événement
 let cheeseNumber; // Nombre de variétés de fromages
 let cheeseQtyPP; // quantité par personne
 let cheeseTypes = []; // types de variétés de fromages
-let nbPersonnes;
+let nbPersonnes; // nombre de personne prévues
 
-let fromagesPersilles = [
-    {
-      "id": "fp1",
-      "lc": "fr",
-      "product_name": "Roquefort Société Cave Saveur",
-      "generic_name": "ROQUEFORT AOP au lait cru de brebis",
-      "brand": "Société,Lactalis",
-      "categories": "Produits laitiers, Produits fermentés, Produits laitiers fermentés, Fromages, Fromages à pâte persillée, Fromages à pâte persillée français, Fromages de brebis, Roqueforts",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:blue-veined-cheeses,en:french-blue-veined-cheeses,en:sheep-s-milk-cheeses,en:roquefort-cheeses",
-      "labels": "AOP, Triman, en:aop",
-      "labels_tags": "en:pdo,fr:triman,en:aop",
-      "origins": "France",
-      "ingredients_text": "Roquefort AOP au lait cru de brebis",
-      "allergens": "Lait",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "https://www.roquefort-societe.com/produit/roquefort-aop-societe-1863-cave-saveur/",
-      "nutriscore_grade": "e",
-      "nutriscore_score": 19,
-      "ecoscore_grade": "c",
-      "ecoscore_score": 53
-    },
-    {
-      "id": "fp2",
-      "lc": "fr",
-      "product_name": "Bleu de laqueuille",
-      "generic_name": "",
-      "brand": "",
-      "categories": "Produits laitiers, Produits fermentés, Produits laitiers fermentés, Fromages, Fromages à pâte persillée, Fromages à pâte persillée français, Bleus de Laqueuille",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:blue-veined-cheeses,en:french-blue-veined-cheeses,fr:bleus-de-laqueuille",
-      "labels": "",
-      "labels_tags": "",
-      "origins": "",
-      "ingredients_text": "",
-      "allergens": "",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "",
-      "nutriscore_grade": "e",
-      "nutriscore_score": 19,
-      "ecoscore_grade": "d",
-      "ecoscore_score": 34
-    },
-    {
-      "id": "fp3",
-      "lc": "fr",
-      "product_name": "Roquefort",
-      "generic_name": "",
-      "brand": "",
-      "categories": "Produits laitiers, Produits fermentés, Produits laitiers fermentés, Fromages, Fromages à pâte persillée, Fromages à pâte persillée français, Fromages de brebis, Roqueforts",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:blue-veined-cheeses,en:french-blue-veined-cheeses,en:sheep-s-milk-cheeses,en:roquefort-cheeses",
-      "labels": "",
-      "labels_tags": "",
-      "origins": "",
-      "ingredients_text": "",
-      "allergens": "",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "",
-      "nutriscore_grade": "e",
-      "nutriscore_score": 19,
-      "ecoscore_grade": "d",
-      "ecoscore_score": 24
-    },
-    {
-      "id": "fp4",
-      "lc": "en",
-      "product_name": "bleu au lait de vache",
-      "generic_name": "Fromage à pâte persillée au lait pasteurisé",
-      "brand": "Lys bleu,Aldi",
-      "categories": "Dairies, Fermented foods, Fermented milk products, Cheeses, Blue-veined cheeses, French blue-veined cheeses",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:blue-veined-cheeses,en:french-blue-veined-cheeses",
-      "labels": "",
-      "labels_tags": "",
-      "origins": "France",
-      "ingredients_text": "_Lait_ pasteurisé (92%) (origine France), _Crème_ pasteurisée (6%) (origine France), sel, présure, ferments (contient du _lait_),",
-      "allergens": "Milk",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "",
-      "nutriscore_grade": "d",
-      "nutriscore_score": 14,
-      "ecoscore_grade": "c",
-      "ecoscore_score": 58
-    },
-    {
-      "id": "fp5",
-      "lc": "fr",
-      "product_name": "Fourme D'ambert AOP ~2,8kg",
-      "generic_name": "",
-      "brand": "",
-      "categories": "Produits laitiers, Produits fermentés, Produits laitiers fermentés, Fromages, Fromages à pâte persillée, Fromages de France, Fromages à pâte persillée français, Fourme d'Ambert",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:blue-veined-cheeses,en:french-cheeses,en:french-blue-veined-cheeses,en:fourme-d-ambert-cheese-from-cow-s-milk",
-      "labels": "AOP",
-      "labels_tags": "en:pdo",
-      "origins": "France",
-      "ingredients_text": "",
-      "allergens": "",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "",
-      "nutriscore_grade": "",
-      "nutriscore_score": null,
-      "ecoscore_grade": "c",
-      "ecoscore_score": 58
-    },
-    {
-      "id": "fp6",
-      "lc": "fr",
-      "product_name": "Roquefort Papillon AOP 1/2 ~1,3kg",
-      "generic_name": "",
-      "brand": "",
-      "categories": "Produits laitiers, Produits fermentés, Produits laitiers fermentés, Fromages, Fromages à pâte persillée, Fromages à pâte persillée français, Fromages de brebis, Roqueforts",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:blue-veined-cheeses,en:french-blue-veined-cheeses,en:sheep-s-milk-cheeses,en:roquefort-cheeses",
-      "labels": "AOP",
-      "labels_tags": "en:pdo",
-      "origins": "France",
-      "ingredients_text": "",
-      "allergens": "",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "",
-      "nutriscore_grade": "",
-      "nutriscore_score": null,
-      "ecoscore_grade": "c",
-      "ecoscore_score": 48
-    },
-    {
-      "id": "fp7",
-      "lc": "fr",
-      "product_name": "Bleu D'Auvergne AOP 1/2 Pain ~1,2kg",
-      "generic_name": "",
-      "brand": "",
-      "categories": "Produits laitiers, Produits fermentés, Produits laitiers fermentés, Fromages, Fromages de vache, Fromages à pâte persillée, Fromages à pâte persillée français, Bleus d'Auvergne",
-      "categories_tags": "en:dairies,en:fermented-foods,en:fermented-milk-products,en:cheeses,en:cow-cheeses,en:blue-veined-cheeses,en:french-blue-veined-cheeses,en:auvergne-blue-cheese-from-cow-s-milk",
-      "labels": "AOP",
-      "labels_tags": "en:pdo",
-      "origins": "France",
-      "ingredients_text": "",
-      "allergens": "",
-      "conservation_conditions": "",
-      "warning": "",
-      "link": "",
-      "nutriscore_grade": "",
-      "nutriscore_score": null,
-      "ecoscore_grade": "c",
-      "ecoscore_score": 58
-    }
-]
+// Instanciation des listes de fromages par type
+let blue_cheeses = [];
+let fresh_cheeses = [];
+let uncooked_pressed_cheeses = [];
+let cooked_pressed_cheeses = [];
+let rind_washed_cheeses = [];
+let soft_ripened_cheeses = [];
+let mixed_cheeses = [];
+let stretched_curd_cheeses = [];
 
-console.log(fromagesPersilles[0]);
 
+// Fonction de génération d'un entier random inférieur au nombre spécifié
 function getRandomInt(max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max +1));
@@ -163,6 +23,40 @@ function getRandomInt(max) {
 
 
 
+// Tri des fromages et stockage dans les listes associées
+for (var i=0; i<fromages.length; i++){
+    let currentCheese = fromages[i];
+
+    switch (currentCheese.type_fromage){
+        case 'blue-cheese':
+            blue_cheeses.push(currentCheese);
+            break;
+        case 'fresh-cheese':
+            fresh_cheeses.push(currentCheese);
+            break;
+        case 'uncooked-pressed-cheese':
+            uncooked_pressed_cheeses.push(currentCheese);
+            break;
+        case 'cooked-pressed-cheese':
+            cooked_pressed_cheeses.push(currentCheese);
+            break;
+        case 'rind-washed-cheese':
+            rind_washed_cheeses.push(currentCheese);
+            break;
+        case 'soft-ripened-cheese':
+            soft_ripened_cheeses.push(currentCheese);
+            break;
+        case 'mixed-cheese':
+            mixed_cheeses.push(currentCheese);
+            break;
+        case 'stretched-curd-cheese':
+            stretched_curd_cheeses.push(currentCheese);
+            break;
+    }
+
+}
+
+// Fonctionsde configuration du plateau
 function selected_event_type(eventType){
     // défini le type d'événement ainsi que la quantité par personne
     switch (eventType){
@@ -198,7 +92,6 @@ function cheese_types_quantity(value){
         cheeseNumber = 15;
 } 
 
-
 function check_cheese_types_selection(){
     var checkboxes = document.getElementsByName('cheese-type');
     var cheeses = [];
@@ -210,48 +103,77 @@ function check_cheese_types_selection(){
     cheeseTypes = cheeses;
 }
 
-
+// Fonctions de générations du plateau
 function generate_cheese_type_box(cheese_type){
     var template = document.querySelector("#cheese-type-bloc");
     var box = document.importNode(template.content, true);
-
-    var header = box.querySelector('h1.bloc-header');
-    var list = box.querySelector('div.bloc-cheese-list');
+    
+    var header = box.querySelector('h1.box-header');
+    var list = box.querySelector('div.box-cheese-list');
 
     switch (cheese_type){
         case 'blue-cheese':
             header.textContent = "Fromages persillés";
-            for (var i = 0; i<3; i++);
+            for (var i = 0; i<3; i++){
                 let card = generate_cheese_card('blue-cheese');
-                console.log(card);
                 list.appendChild(card);
+            }
             break
         
         case 'fresh-cheese':
             header.textContent = "Fromages frais";
-            break
-        case 'goat-cheese':
-            header.textContent = "Fromages de chèvre";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('fresh-cheese');
+                list.appendChild(card);
+            }
             break
 
-        case 'pressed-cheese':
+        case 'mixed-cheese':
+            header.textContent = "Fromages forts";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('mixed-cheese');
+                list.appendChild(card);
+            }
+            break
+
+        case 'cooked-pressed-cheese':
             header.textContent = "Fromages pressés cuits";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('cooked-pressed-cheese');
+                list.appendChild(card);
+            }
             break
 
         case 'rind-washed-cheese':
             header.textContent = "Fromages à croûte lavée";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('rind-washed-cheese');
+                list.appendChild(card);
+            }
             break
 
-        case 'sheep-cheese':
-            header.textContent = "Fromages de brebis";
+        case 'stretched-curd-cheese':
+            header.textContent = "Fromages à pâte filée";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('stretched-curd-cheese');
+                list.appendChild(card);
+            }
             break
 
         case 'soft-ripened-cheese':
             header.textContent = "Fromages à croûte fleurie";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('soft-ripened-cheese');
+                list.appendChild(card);
+            }
             break
 
         case 'uncooked-pressed-cheese':
             header.textContent = "Fromages pressés non cuits";
+            for (var i = 0; i<3; i++){
+                let card = generate_cheese_card('uncooked-pressed-cheese');
+                list.appendChild(card);
+            }
             break
     }
     return box
@@ -260,50 +182,62 @@ function generate_cheese_type_box(cheese_type){
 
 function generate_cheese_card(cheese_type){
 
-    var template = document.querySelector("#cheese-card");
+    var template = document.querySelector("#cheese-example");
     var card = document.importNode(template.content, true);
 
-    var name = card.querySelector('h1.cheese-card-header');
-    var country = card.querySelector('p.cheese-card-country');
+    var name = card.querySelector('h2.cheese-card-header');
+    var origin = card.querySelector('p.cheese-card-origin');
 
     switch (cheese_type){
         case 'blue-cheese':      
-            var randomCheese = fromagesPersilles[getRandomInt(fromagesPersilles.length)];
-            name = randomCheese.product_name;
-            country = randomCheese.origins;
+            var randomCheese = blue_cheeses[getRandomInt(blue_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
             return card;        
 
         case 'fresh-cheese':
-            header.textContent = "Fromages frais";
-            break
-        case 'goat-cheese':
-            header.textContent = "Fromages de chèvre";
-            break
+            var randomCheese = fresh_cheeses[getRandomInt(fresh_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card; 
 
-        case 'pressed-cheese':
-            header.textContent = "Fromages pressés cuits";
-            break
+        case 'mixed-cheese':
+            var randomCheese = mixed_cheeses[getRandomInt(mixed_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card; 
+
+        case 'cooked-pressed-cheese':
+            var randomCheese = cooked_pressed_cheeses[getRandomInt(cooked_pressed_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card; 
 
         case 'rind-washed-cheese':
-            header.textContent = "Fromages à croûte lavée";
-            break
+            var randomCheese = rind_washed_cheeses[getRandomInt(rind_washed_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card;
 
-        case 'sheep-cheese':
-            header.textContent = "Fromages de brebis";
-            break
+        case 'stretched-curd-cheese':
+            var randomCheese = stretched_curd_cheeses[getRandomInt(stretched_curd_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card;
 
         case 'soft-ripened-cheese':
-            header.textContent = "Fromages à croûte fleurie";
-            break
+            var randomCheese = soft_ripened_cheeses[getRandomInt(soft_ripened_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card;
 
         case 'uncooked-pressed-cheese':
-            header.textContent = "Fromages pressés non cuits";
-            break
+            var randomCheese = uncooked_pressed_cheeses[getRandomInt(uncooked_pressed_cheeses.length)];
+            name.textContent = randomCheese.nom;
+            origin.textContent = randomCheese.departement;
+            return card;
     }
 }
-
-
-
 
 function generatePlateau(){
     // Calcul de la quantité totale de fromage à prévoir
@@ -325,5 +259,6 @@ function generatePlateau(){
     
 }
 
+// Déclenchement de la génération avec l'événement du bouton
 btnGeneration = document.getElementById('btn-generation-plateau');
 btnGeneration.addEventListener('click', generatePlateau);
