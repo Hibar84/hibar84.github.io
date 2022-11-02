db0b188e0c99dc5ccdffea69838c72b3a5e85821
Lucas GUILHEN <lucas.guilhen67@gmail.com>
2022-11-02 18:06:59 +0100

    /

--- projets/mes-gares/nuxt.config.ts
+++ projets/mes-gares/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   router: {
-    base: '/hibar84.github.io/projets/mes-gares/'
+    base: '/hibar84.github.io/'
   },
 
   target: 'static',
