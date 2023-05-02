f386ea5a981466544fea40bfc485d14fb6839a87
Lucas GUILHEN <lucas.guilhen67@gmail.com>
2023-05-02 15:34:59 +0200

    peaufinage add recettes et theme

--- pages/recettes/add.vue
+++ pages/recettes/add.vue
@@ -48,22 +48,37 @@
     
     <div class="p-4 flex flex-col items-center gap-3">
       <h1 class="text-3xl font-semibold text-center">Ajouter une recette</h1>
-      <input type="text" placeholder="Nom de la recette" class="input input-bordered w-full max-w-md" v-model="nouvelleRecette.title"/>
+      
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text">Recette</span>
+        </label>
+        <input type="text" placeholder="Nom de la recette" class="input input-bordered w-full max-w-md mb-3" v-model="nouvelleRecette.title"/>
+        <input type="text" placeholder="Description" class="input input-bordered w-full max-w-md" v-model="nouvelleRecette.desc"/>
+      </div>
 
-      <input type="text" placeholder="Description" class="input input-bordered w-full max-w-md" v-model="nouvelleRecette.desc"/>
-      
-      <select class="select select-bordered w-full max-w-md" v-model="nouvelleRecette.categorie">
-        <option disabled unselectable>Catégorie</option>
-        <option>Plats</option>
-        <option>Desserts</option>
-        <option>Pains</option>
-      </select>
-      
-      <p class="mb-1">Nombre de personnes (ou portions): {{ nouvelleRecette.nb_personnes }}</p>
-      
-      <input type="range" min="0" max="20" class="range range-xs w-full max-w-md" v-model="nouvelleRecette.nb_personnes" />
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text">Catégorie</span>
+        </label>
+        <select class="select select-bordered w-full max-w-md" v-model="nouvelleRecette.categorie">
+          <option>Plats</option>
+          <option>Desserts</option>
+          <option>Pains</option>
+        </select>
+      </div>
+    
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text">Nombre de personnes (ou portions): {{ nouvelleRecette.nb_personnes }}</span>
+        </label>
+        <input type="range" min="0" max="20" class="range range-xs w-full max-w-md" v-model="nouvelleRecette.nb_personnes" />
+      </div>
 
-      <div class="form-control">
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text">Ingrédients</span>
+        </label>
         <div class="input-group">
           <input type="text" placeholder="Ingrédient" class="input input-bordered w-44" v-model="nouvelIngredient.desc">
           <input type="text" placeholder="Quantité" class="input input-bordered w-24" v-model="nouvelIngredient.quantite">
@@ -73,14 +88,19 @@
         </div>
       </div>
 
-      <ul class="p-4 list-disc max-w-md">
+      <ul class="list-disc max-w-md">
         <li v-for="ingredient in nouvelleRecette.ingredients" :key="ingredient.desc">
           {{ ingredient.desc }}: {{ ingredient.quantite === "NA" ? "" : ingredient.quantite }}{{ ingredient.unit }}
         </li>
       </ul>
-
-      <textarea placeholder="Instructions" class="textarea textarea-bordered textarea-lg text-base w-full max-w-md" v-model="instructions_bloc"/>
       
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text">Instructions</span>
+        </label>
+        <textarea placeholder="Instructions" class="textarea textarea-bordered textarea-lg text-base w-full max-w-md" v-model="instructions_bloc"/>
+      </div> 
+
       <button class="btn" @click="creerRecette" :disabled="loading">Créer</button>
     </div> 
   </div>
